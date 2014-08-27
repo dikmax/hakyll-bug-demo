8ac83b14cc706683ee08bb26de9f6fc89624b4ca
Maxim Dikun <dikmax@gmail.com>
2014-08-27 21:58:37 +0300

    Initial commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ghc --make -threaded site.hs
---
+++ site.hs
@@ -0,0 +1,18 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "less/*.less" $
+        compile getResourceBody
+
+    d <- makePatternDependency "less/**"
+    rulesExtraDependencies [d] $ create ["css/style.css"] $ do
+        route idRoute
+        compile $ loadBody "less/style.less"
+            >>= makeItem
+            >>= withItemBody
+              (unixFilter "lessc" ["--clean-css","-O2", "--include-path=less","-"])
